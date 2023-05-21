1eb688ca736edcda485d34c7237333b41a6de58c
ton8080 <washingtonzxy@gmail.com>
2023-05-21 14:11:57 -0300

    update

--- hospital_liberdade-feature-identacao-login/javascript/menu.js
+++
@@ -1,8 +0,0 @@
-const menuHamburguer = document.querySelector('.menu-hamburguer');
-const botaoHamburguer = document.querySelector('.menu-hamburguer span');
-const menuBotoes = document.querySelector('#botoes');
-
-menuHamburguer.addEventListener('click', function() {
-  menuHamburguer.classList.toggle('active');
-  menuBotoes.classList.toggle('active');
-});
